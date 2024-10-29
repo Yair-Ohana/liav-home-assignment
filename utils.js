6ecb6fffb10f2c2b0c9c067aa7a1454a05883545
liav <liav1232111@gmail.com>
2024-10-29 19:01:28 +0200

    last comment

--- utils.js
+++ utils.js
@@ -1,6 +1,5 @@
 export function addToFavorites(movieId, movieTitle, posterPath) {
-  const favoriteMovies =
-    JSON.parse(localStorage.getItem("favoriteMovies")) || [];
+  const favoriteMovies = JSON.parse(localStorage.getItem("favoriteMovies")) || [];
 
   if (!favoriteMovies.some((favMovie) => favMovie.id == movieId)) {
     const movie = { id: movieId, title: movieTitle, poster_path: posterPath };
